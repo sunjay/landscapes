295bc0f0e13e1c1e27771aa4082b7cea51687188
Sunjay Varma <varma.sunjay@gmail.com>
2016-12-25 16:13:53 -0700

    Tweaking default parameters and rendering

--- src/renderer.js
+++ src/renderer.js
@@ -9,26 +9,33 @@ export function renderPath(ctx, width, height, path) {
   const yCoord = (v) => v * yaxis.factor + yaxis.ref;
 
   ctx.lineWidth = 2;
-  ctx.strokeStyle = '#2196f3';
+  ctx.strokeStyle = '#3e2723';
+  ctx.fillStyle = '#795548';
 
   const step = width / (path.length - 1);
-  drawPath(ctx, path.map((y, i) => ({x: xCoord(step * i), y: yCoord(y)})));
+  const coords = path.map((y, i) => ({x: xCoord(step * i), y: yCoord(y)}));
+  drawPath(ctx, coords, width, height);
 }
 
 export function renderProgress(ctx, width, height, completed, total) {
-  const size = 28;
+  const size = 24;
   const padding = 10;
   ctx.font = `${size}px "Open Sans"`;
-  ctx.fillText(`${completed} / ${total}`, padding, height - size/2);
+  ctx.textAlign = 'end';
+  ctx.fillStyle = 'black';
+  ctx.fillText(`${completed} / ${total}`, width - padding, padding * 2 + size/2);
 }
 
-function drawPath(ctx, coordinates) {
+function drawPath(ctx, coordinates, width, height) {
   ctx.beginPath();
-  ctx.moveTo(coordinates[0].x, coordinates[0].y);
+  ctx.moveTo(0, height);
 
-  for (const {x, y} of coordinates.slice(1)) {
+  for (const {x, y} of coordinates) {
     ctx.lineTo(x, y);
   }
 
+  ctx.lineTo(width, height);
+  ctx.lineTo(0, height);
+  ctx.fill();
   ctx.stroke();
 }
